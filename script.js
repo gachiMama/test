e027e109e77357968745002bc6a655f78413e24d
gachiMama <97821582+gachiMama@users.noreply.github.com>
2022-01-23 10:31:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//var w = document.querySelector("#w") 
+
+//console.log(w)
+
+
+//var btn = document.querySelector("#btn")
+
+
+
+//console.log(btn)
+
+
+
+//btn.onclick = function(){
+ 
+//  w.style.background = `blue`
+
+//}
+
+
+
+//if (age == 0) {
+    //alert("Интернет в 0 лет")
+//}
+//  alert("Пошел нахер малолетка")
+//}
+
+//else {
+  //alert("Привет незнакомец")
+//}
+
+//switch (age) {
+    //case 0: alert(`Интернет в нуль лет`)
+             //break;
+
+    //case 18: alert(`Пошел нахер малолетка`)
+             //break;
+
+//default:alert("Привет незнакомец")
+     // break;
+//}
+
+var age = +prompt("Сколько вам лет?")
+
+age < 18 ?  alert(`Пошел нахер малолетка`) : alert(`Добро пожаловать`)
+age == 20 ? alert (`ну ты и старик`) : alert(`Привет милипиздрик`)
